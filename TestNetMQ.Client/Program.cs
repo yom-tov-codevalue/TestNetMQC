fa1047820dd5f30ad687c0d619953ad2a4996313
BENIAMINOV YOM-TOV <YOMTOVB@WWEST.LOCAL>
2021-04-07 16:28:09 +0300

    Add project files.

---
+++ TestNetMQ.Client/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using NetMQ;
+using NetMQ.Sockets;
+using Google.Protobuf;
+using Shared.Message;
+
+namespace TestNetMQ.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NetMQRuntime mqRuntime = new NetMQRuntime();
+            mqRuntime.Run(Run(args));
+        }
+
+        static async Task Run(string[] args)
+        {
+            Console.WriteLine("Starting ZMQ Client");
+            if (args == null || args.Length < 1)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Usage: ./{0}", AppDomain.CurrentDomain.FriendlyName);
+                Console.WriteLine();
+                args = new[] { "Hello" };
+            }
+
+            var arg = args[0];
+
+            Message msg = new()
+            {
+                Data = arg,
+                Id = 1
+            };
+
+            using var client = new RequestSocket(@">tcp://localhost:5556");
+
+            var bufferedMsg = msg.ToByteArray();
+
+            client.SendFrame(bufferedMsg);
+
+            (byte[], bool) socketMsgResponse;
+
+            while (true)
+            {
+                socketMsgResponse = await client.ReceiveFrameBytesAsync().ConfigureAwait(false);
+                if (!socketMsgResponse.Item2)
+                {
+                    break;
+                }
+            }
+
+            var receivedMsg = Message.Parser.ParseFrom(socketMsgResponse.Item1);
+
+            Console.WriteLine("From Server: {0} - {1}", receivedMsg.Id, receivedMsg.Data);
+
+            Console.WriteLine();
+            Console.Write("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestNetMQ.Server/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Google.Protobuf;
+using NetMQ;
+using NetMQ.Sockets;
+using Shared.Message;
+
+namespace TestNetMQ.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using NetMQRuntime mqRuntime = new NetMQRuntime();
+            mqRuntime.Run(Run());
+        }
+
+        static async Task Run()
+        {
+            Console.WriteLine("Starting ZMQ Server");
+
+            using var server = new ResponseSocket(@"@tcp://localhost:5556");
+
+            (byte[], bool) socketMsgRequest;
+
+            while (true)
+            {
+                socketMsgRequest = await server.ReceiveFrameBytesAsync().ConfigureAwait(false);
+                if (!socketMsgRequest.Item2)
+                {
+                    break;
+                }
+            }
+
+            var receivedMsg = Message.Parser.ParseFrom(socketMsgRequest.Item1);
+
+            Console.WriteLine("From Client: {0} - {1}", receivedMsg.Id, receivedMsg.Data);
+
+            server.SendFrame(
+                (new Message()
+                {
+                    Id = 2,
+                    Data = "Response from server",
+                })
+                .ToByteArray());
+        }
+    }
+}
